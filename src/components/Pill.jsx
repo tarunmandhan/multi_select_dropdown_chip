021ef067b0830f3ff9f660f50eee1d293e1dfb22
tarunmandhan <mandhantarun@gmail.com>
2025-02-04 22:22:18 +0530

    fix delete functionality , Now works only click on x

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Pill = ({ image, text, onClick }) => {
   return (
     <>
-      <span className="user-pill" onClick={onClick}>
+      <span className="user-pill">
         <img src={image} alt={text} />
-        <span>{text} &times;</span>
+        <span>
+          {text} <span onClick={onClick}>&times;</span>
+        </span>
       </span>
     </>
   );
